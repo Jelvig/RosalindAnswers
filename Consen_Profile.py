8776ab53598611cf6b651a4c4bdeeab0dc1b8761
Jelvig <111405495+Jelvig@users.noreply.github.com>
2023-02-23 13:49:04 -0800

    Update Consen_Profile.py

--- Consen_Profile.py
+++ Consen_Profile.py
@@ -45,3 +45,56 @@ print('A: ' + ' '.join(str(x) for x in A))
 print('C: ' + ' '.join(str(x) for x in C))
 print('G: ' + ' '.join(str(x) for x in G))
 print('T: ' + ' '.join(str(x) for x in T))
+
+
+from collections import Counter
+
+# Define the DNA sequences as a list of strings
+dna_strings = []
+sequence = ''
+path = r"C:\Users\jelvig\Downloads\rosalind_cons.txt"
+with open(path, 'r') as f:
+    for line in f:
+        if line[0] == '>':
+            if sequence:
+                dna_strings.append(sequence)
+            sequence=''
+        else:
+            sequence+=line.replace('\n', '')
+    dna_strings.append(sequence)
+
+
+
+
+"""Best answer to question"""
+# # Get the length of the DNA sequences
+# n = len(dna_strings[0])
+
+# # Initialize the profile matrix as a dictionary of Counters
+# profile_matrix = {
+#     'A': Counter(),
+#     'C': Counter(),
+#     'G': Counter(),
+#     'T': Counter()
+# }
+
+# # Update the profile matrix with the counts of each nucleotide at each position
+# for i in range(n):
+#     for dna_string in dna_strings:
+#         profile_matrix[dna_string[i]][i] += 1
+
+# # Print the profile matrix
+# for nucleotide in ['A', 'C', 'G', 'T']:
+#     print(nucleotide + ':', end=' ')
+#     for count in profile_matrix[nucleotide].values():
+#         print(count, end=' ')
+#     print()
+
+# # Compute the consensus sequence by selecting the most common nucleotide at each position
+# consensus = ''
+# for i in range(n):
+#     counts = [profile_matrix[nucleotide][i] for nucleotide in ['A', 'C', 'G', 'T']]
+#     consensus += max(zip(counts, ['A', 'C', 'G', 'T']))[1]
+
+# # Print the consensus sequence
+# print('Consensus:', consensus)
